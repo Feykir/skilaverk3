99a1348111f71f5368249fe49a7a7abb281d8788
Feykir <kollit90@gmail.com>
2021-10-10 19:31:41 +0000

    breytti num>0 i num>1 og thetta virkar rett nuna

--- bin/spurning4.dart
+++ bin/spurning4.dart
@@ -6,12 +6,11 @@ void main(List<String> arguments) {
   print('Enter a number:');
   double num = double.parse(stdin.readLineSync());
 
-  while (num > 0) {
+  while (num > 1) {
     num = num / 10;
     digit++;
 
   }
   print('Total digits: $digit');
-  //Veit ekki afhverju en forritið byrjar alltaf að telja frá 323, en ef ég slæ inn 0 þá kemur 0 :S
-  //Fyrir utan að það byrjar að telja frá 323 þá virkar allt eðlilega, ég prufaði að slá inn 10 tölur og endaði á 333
+
 }
